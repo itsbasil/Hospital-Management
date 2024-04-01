c7f34dfc5af20a1785dc169de8fa808c0232b26a
itsbasil <itsbasilgeorge@gmail.com>
2024-04-01 22:59:20 +0530

    #patientReg page connected to admin,make changes on admin.py,forms.py,models.py & views.py

--- myenv/myproject/myapp/models.py
+++ myenv/myproject/myapp/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
 # Create your models here.
+class Patient(models.Model):
+    patientname=models.CharField(max_length=150)
+    dob=models.CharField(max_length=150)
+    address=models.CharField(max_length=150)
+    age=models.CharField(max_length=100)
+    gender=models.CharField(max_length=100)
+    contactnum=models.CharField(max_length=100)
+    username=models.CharField(max_length=150)
+    password=models.CharField(max_length=150)
+    password1=models.CharField(max_length=150)
+    def __str__(self):
+        return self.patientname
--- myenv/myproject/myapp/views.py
+++ myenv/myproject/myapp/views.py
@@ -1,13 +1,31 @@
-from django.shortcuts import render
+from django.contrib.auth.models import User,auth
+from django.shortcuts import render,redirect
+from.forms import PatientForm
+from.models import Patient
+from django.http import HttpResponse
+
 
 # Create your views here.
-
-
 def index(request):
-    return render (request, 'index.html')
+    return render(request,'index.html')
+
 def patientReg(request):
-    return render (request, 'patientReg.html')
+    if request.method == "POST":
+        username=request.POST['username']
+        password = request.POST['password']
+        password1 = request.POST['password1']
+        if password == password1:
+            if User.objects.filter(username=username).exists():
+                return redirect('patientReg')
+            user = User.objects.create_user(username=username, password=password)
+            user.save()
+        else:
+            return redirect('patientReg')
+        return redirect('/')
+    else:
+        return render(request,'patientReg.html')
+
 def patientLog(request):
-    return render (request, 'patientLog.html')
+    return render(request,'patientLog.html')
 def appointment(request):
-    return render (request, 'appointment.html')
+    return render(request,'appointment.html')
