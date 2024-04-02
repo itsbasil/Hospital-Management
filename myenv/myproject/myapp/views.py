fa02806a4fd07e614f0ab820bbf50ebe48759baf
itsbasil <itsbasilgeorge@gmail.com>
2024-04-02 22:52:05 +0530

    #patientReg page registration bug fixed

--- myenv/myproject/myapp/views.py
+++ myenv/myproject/myapp/views.py
@@ -4,7 +4,6 @@ from.forms import PatientForm
 from.models import Patient
 from django.http import HttpResponse
 
-
 # Create your views here.
 def index(request):
     return render(request,'index.html')
